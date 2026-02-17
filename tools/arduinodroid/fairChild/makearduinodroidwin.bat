88de4d0afac04ac30debe9c8024a73c6fcc8b30e
ackerman <jaime_jj@hotmail.com>
2026-02-17 14:27:46 +0100

    Correct path

--- tools/arduinodroid/fairChild/makearduinodroidwin.bat
+++ tools/arduinodroid/fairChild/makearduinodroidwin.bat
@@ -2,19 +2,19 @@
 echo Convert ARDUINODROID PROJECT DATA (WIN32)
 pause
 echo Copy *.cpp (Copiar *.cpp)
-copy/Y ..\..\..\TinyFairChildttgovga32\fairChild\*.cpp
+copy/Y ..\..\..\ESP32\TinyFairChildttgovga32\fairChild\*.cpp
 echo Copy *.h (Copiar *.h)
-copy/Y ..\..\..\TinyFairChildttgovga32\fairChild\*.h
+copy/Y ..\..\..\ESP32\TinyFairChildttgovga32\fairChild\*.h
 echo Copy fairChild.ino (Copiar fairChild.ino)
-copy/Y ..\..\..\TinyFairChildttgovga32\fairChild\fairChild.ino
+copy/Y ..\..\..\ESP32\TinyFairChildttgovga32\fairChild\fairChild.ino
 echo Copy gbcart.h (Copiar gbcart.h)
-copy/Y ..\..\..\TinyFairChildttgovga32\fairChild\dataFlash\gbcart.h
+copy/Y ..\..\..\ESP32\TinyFairChildttgovga32\fairChild\dataFlash\gbcart.h
 echo Copy gbCompileDataOpt.h (Copiar gbCompileDataOpt.h)
-copy/Y ..\..\..\TinyFairChildttgovga32\fairChild\dataFlash\gbCompileDataOpt.h
+copy/Y ..\..\..\ESP32\TinyFairChildttgovga32\fairChild\dataFlash\gbCompileDataOpt.h
 echo Copy Chf (Copiar Chf)
-copy/Y ..\..\..\TinyFairChildttgovga32\fairChild\dataFlash\cart\*.h
+copy/Y ..\..\..\ESP32\TinyFairChildttgovga32\fairChild\dataFlash\cart\*.h
 echo Copy ROMs (Copiar ROMs)
-copy/Y ..\..\..\TinyFairChildttgovga32\fairChild\dataFlash\bios\*.h
+copy/Y ..\..\..\ESP32\TinyFairChildttgovga32\fairChild\dataFlash\bios\*.h
 echo Copy Options ARDUINODROID
 copy/Y notdelete\gbCompileOpt.h
 pause
